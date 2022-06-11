b49ab12b79e154f4fc0ee353b1ff4eec3e4cfe6d
jula1717 <julaplocienniczak@gmail.com>
2022-06-11 12:11:28 +0200

    Gui

--- app/src/main/java/com/example/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.eggtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar timerBar;
+    ImageView imgEgg;
+    TextView txtTimer;
+    Button btnStart,btnStop;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timerBar = (SeekBar) findViewById(R.id.timeBar);
+        imgEgg = (ImageView) findViewById(R.id.imgEgg);
+        txtTimer = (TextView) findViewById(R.id.txtTimer);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+
+
+
+
     }
 }
