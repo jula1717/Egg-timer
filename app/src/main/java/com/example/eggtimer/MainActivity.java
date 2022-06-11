f82d1bc6050274d7baefc2cdfa5312b7ac26364d
jula1717 <julaplocienniczak@gmail.com>
2022-06-11 12:41:54 +0200

    TextView follows SeekBar, time format

--- app/src/main/java/com/example/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.eggtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -25,8 +27,51 @@ public class MainActivity extends AppCompatActivity {
         txtTimer = (TextView) findViewById(R.id.txtTimer);
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStop = (Button) findViewById(R.id.btnStop);
+        imgEgg.setImageResource(R.drawable.egg);
+        timerBar.setMax(300);
+        timerBar.setProgress(0);
+        timerBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int minutes = (int) i/60;
+                int seconds = i%60;
+                String sSeconds=seconds<10?"0" + Integer.toString(seconds):Integer.toString(seconds);
+                txtTimer.setText(Integer.toString(minutes)+":"+sSeconds);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
+//        btnStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                new CountDownTimer(10000,1000)
+//                {
+//
+//                    @Override
+//                    public void onTick(long l) {
+//                        //countdown is counting down (every second - first argument)
+//                        System.out.println("Seconds left: "+String.valueOf(l/1000));
+//                    }
+//
+//                    @Override
+//                    public void onFinish() {
+//                        //counter is finished! (after 10 seconds - second argment)
+//                        System.out.println("Countdown timer finished");
+//
+//                    }
+//                }.start();
+//            }
+//        });
 
 
     }
